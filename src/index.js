0f9865a274e67faf351336febc38c4737fd30449
axetroy <troy450409405@gmail.com>
2017-02-09 03:08:44 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -9,6 +9,11 @@ const env = {
   prod: process.env.NODE_ENV === "production"
 };
 
+const github = {
+  clientId: 'b8257841dd7ca5eef2aa',
+  clinetSecret: '4da33dd6fcb0a01d395945ad18613ecf9c12079e'
+};
+
 const icon = {
   github: createSvg({
     path: 'M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z',
@@ -114,7 +119,7 @@ async function packageList(): Promise {
       return console.error(`${repoInfo.href} is not a github repository`);
     }
 
-    let gitRepoInfo = await JSONIfyHttpResponse(http.get(`https://api.github.com/repos/${repoInfo.repo}?client_id=b8257841dd7ca5eef2aa&client_secret=4da33dd6fcb0a01d395945ad18613ecf9c12079e`));
+    let gitRepoInfo = await getGithubRepoInfo(repoInfo.repo);
 
     env.dev && ele.setAttribute('github-info', JSON.stringify(gitRepoInfo));
 
@@ -186,4 +191,13 @@ function createSvg({path, width, height}) {
 
 function numFormat(num: number): string {
   return num.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,').replace(/\.\d+/, '');
+}
+
+/**
+ * get github repo info
+ * @param githubRepoPath  example: /axetroy/git-clone/cli
+ * @returns {*}
+ */
+function getGithubRepoInfo(githubRepoPath) {
+  return JSONIfyHttpResponse(http.get(`https://api.github.com/repos/${githubRepoPath}?client_id=${github.clientID}&client_secret=${github.clinetSecret}`));
 }
