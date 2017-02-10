cdfe821373ae23878c517b0f0a448e6e6699fd86
axetroy <troy450409405@gmail.com>
2017-02-11 00:23:24 +0800

    feat: 直接解析require路径，点击即可跳转到对应的包

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -3,6 +3,7 @@ import Promise from 'bluebird';
 import github from '../github';
 import Timeago from 'timeago.js';
 import {diffDays} from '../utils';
+import env from '../env';
 
 
 function generateOutdatedWarning(date, diffDays) {
@@ -25,7 +26,12 @@ function generateOutdatedWarning(date, diffDays) {
 }
 
 export default function () {
+
+
   return async function detailPageHandler(): Promise {
+
+    parseCodeBlock();   // parse require('xxx') | import xxx from
+
     const githubLink: HTMLAnchorElement = window.document.querySelector('.box a[href*=github]');
     if (!githubLink) return;
     githubLink.title = githubLink.href;
@@ -51,5 +57,44 @@ export default function () {
 
     window.document.querySelector('.box').insertBefore(container, githubLink.parentNode);
   }
+
+}
+
+function getElementText(ele) {
+  return ele.innerText || ele.textContent;
+}
+
+function parseCodeBlock() {
+  let lines = document.querySelectorAll('.highlight.js pre .line .source');
+
+  lines.forEach(function (line) {
+    if (line.innerHTML.match(/require|from|import/g)) {
+      // console.log(line.innerText);
+      let stringQuotedEle = line.querySelector('.string.quoted') || line.querySelector('.import .string.quoted');
+
+      if (!stringQuotedEle) return;
+
+      const requireName = getElementText(stringQuotedEle).trim().replace(/^['"]|['"]$/g, '');
+
+      if (/^\./.test(requireName)) return;    // 不支持相对路径
+
+      const packageNameMatch = requireName.match(/(^\@[^\/]+\/[^\/]+)|([^\/]+)/) || [];
+      const packageName = packageNameMatch[0];
+
+      if (env.dev) {
+        console.log(requireName + ' >>> ' + packageName);
+      }
+
+      // 正确解析才渲染页面
+      if (packageName) {
+        const childNodes = stringQuotedEle.children;
+        const stringQuotedStartEle = childNodes[0];
+        const stringQuotedEndEle = childNodes[childNodes.length - 1];
+        const packageUrl = `https://www.npmjs.com/package/${packageName}`;
+        stringQuotedEle.innerHTML = stringQuotedStartEle.innerHTML + `<a title="${packageUrl}" style="text-decoration: underline" href="${packageUrl}">${requireName}</a>` + stringQuotedEndEle.innerHTML;
+      }
+
+    }
+  });
 
 }
